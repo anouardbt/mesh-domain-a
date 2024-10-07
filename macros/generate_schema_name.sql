8ed6a5a5c1d6746027640609cec62e5adff8f353
Anouar Hnini <anouar.h@dbtlabs.com>
2024-10-07 09:34:57 +0000

    changes to project

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,20 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- set branch_name = env_var('DBT_CLOUD_GIT_BRANCH') | replace('-', '_') -%}
+    
+    {%- if target.name == 'dev' -%}
+        {%- if custom_schema_name is none -%}
+            {{ branch_name }}_FB_BRANCH
+        {%- else -%}
+            {{ custom_schema_name | trim }}_FB_BRANCH
+        {%- endif -%}
+    {%- else -%}
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+        {%- endif -%}
+    {%- endif -%}
+
+{%- endmacro %}
