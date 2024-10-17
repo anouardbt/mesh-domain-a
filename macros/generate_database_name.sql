ec7119092469f6b3ea70797f508e36b7540acea7
Anouar Hnini <anouar.h@dbtlabs.com>
2024-10-17 12:15:38 +0000

    multi-strategy database

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -3,14 +3,17 @@
     {%- if custom_database_name is not none -%}
         {{ custom_database_name | trim }}
     {%- else -%}
+        {%- set file_path_parts = node.original_file_path.split('/') %}
+        {%- set folder_name = file_path_parts[1] if file_path_parts | length > 2 else project_name %}
+
         {%- if target.name == 'personal_dev' -%}
-            {{ project_name }}_{{ env_var('DBT_CLOUD_GIT_BRANCH', '') | replace('/', '_') | replace('-', '_') }}
+            {{ folder_name }}_{{ env_var('DBT_CLOUD_GIT_BRANCH', '') | replace('/', '_') | replace('-', '_') }}
         {%- elif target.name == 'dev' -%}
-            {{ project_name }}_DEV
+            {{ folder_name }}_DEV
         {%- elif target.name == 'qa' -%}
-            {{ project_name }}_QA
+            {{ folder_name }}_QA
         {%- elif target.name == 'prod' -%}
-            {{ project_name }}_PROD
+            {{ folder_name }}_PROD
         {%- else -%}
             {{ target.database }}
         {%- endif -%}
