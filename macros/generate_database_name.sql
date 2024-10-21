a36d22d7bab6298393e9c81c3512c1e2caf481c0
Anouar Hnini <anouar.h@dbtlabs.com>
2024-10-21 08:58:32 +0000

    added generate_database strateggy

--- macros/generate_database_name.sql
+++ macros/generate_database_name.sql
@@ -1,21 +1,27 @@
-{% macro generate_database_name(custom_database_name=none, node=none) -%}
+{% macro generate_database_name(custom_database_name=none, node=none, pre_hook=False) -%}
     
     {%- if custom_database_name is not none -%}
         {{ custom_database_name | trim }}
     {%- else -%}
-        {%- set file_path_parts = node.original_file_path.split('/') %}
-        {%- set folder_name = file_path_parts[1] if file_path_parts | length > 2 else project_name %}
-
-        {%- if target.name == 'personal_dev' -%}
-            {{ folder_name }}_{{ env_var('DBT_CLOUD_GIT_BRANCH', '') | replace('/', '_') | replace('-', '_') }}
-        {%- elif target.name == 'dev' -%}
-            {{ folder_name }}_DEV
-        {%- elif target.name == 'qa' -%}
-            {{ folder_name }}_QA
-        {%- elif target.name == 'prod' -%}
-            {{ folder_name }}_PROD
+        {%- if pre_hook -%}
+            {# In pre-hooks, node is not available, so default to project_name or a simple folder-based name #}
+            {{ project_name }}
         {%- else -%}
-            {{ target.database }}
+            {# In model compilation, node is available, so proceed with normal logic #}
+            {%- set file_path_parts = node.original_file_path.split('/') %}
+            {%- set folder_name = file_path_parts[1] if file_path_parts | length > 2 else project_name %}
+
+            {%- if target.name == 'personal_dev' -%}
+                {{ folder_name }}_{{ env_var('DBT_CLOUD_GIT_BRANCH', '') | replace('/', '_') | replace('-', '_') }}
+            {%- elif target.name == 'dev' -%}
+                {{ folder_name }}_DEV
+            {%- elif target.name == 'qa' -%}
+                {{ folder_name }}_QA
+            {%- elif target.name == 'prod' -%}
+                {{ folder_name }}_PROD
+            {%- else -%}
+                {{ target.database }}
+            {%- endif -%}
         {%- endif -%}
     {%- endif -%}
 
