06c404db163d3863221d475d4a5c0c881883f118
Anouar Hnini <anouar.h@dbtlabs.com>
2024-11-26 09:58:45 +0000

    test

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,4 +1,4 @@
-{% macro generate_schema_name_2(custom_schema_name, node) -%}
+{% macro generate_schema_name_test(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
     
